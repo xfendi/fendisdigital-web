f4c9a5816f82ddfc0293a6839042f010d9249494
fendziorr <xfendi.channel@gmail.com>
2025-01-21 08:52:06 +0100

    fixed header

--- src/Home/Header.jsx
+++ src/Home/Header.jsx
@@ -5,14 +5,14 @@ const Header = () => {
   return (
     <div className="flex flex-col gap-10 items-center text-center px-24 my-36">
       <div
-        className="text-4xl sm:text-7xl font-semibold"
+        className="text-4xl sm:text-6xl font-semibold"
         style={{ lineHeight: 1.2 }}
       >
         Twórz, Skaluj,{" "}
         <span className="text-blue-500">Zarabiaj – Twoja Biblioteka</span>{" "}
         Wiedzy Web & Biznes
       </div>
-      <div className="text-gray-200 w-full text-xl">
+      <div className="text-gray-400 text-md w-2/3">
         Skontaktuj się ze mną przez DM, a pomogę Ci zaplanować pierwsze kroki.
         Podzielę się sprawdzonymi wskazówkami, które pozwolą Ci zaoszczędzić
         czas i uniknąć błędów. Odbierz także darmowy e-book z praktycznymi
@@ -27,7 +27,7 @@ const Header = () => {
           });
         }}
       >
-        <Link className="p-3 px-7 text-lg font-medium w-max rounded-full transition-all duration-300 bg-blue-500 hover:bg-blue-600">
+        <Link className="p-3 px-7 text-md font-medium w-max rounded-full transition-all duration-300 bg-blue-500 hover:bg-blue-600">
           Darmowe Wprowadzenie
         </Link>
       </div>
